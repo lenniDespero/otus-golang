6886c3667821bd7165860f870ff9fd5293f336cd
astepanov <stepanov.alexey@gtrf.ru>
2019-12-02 13:29:22 +0300

    init commit

---
+++ hw1/go.mod
@@ -0,0 +1,6 @@
+module github.com/lenniDespero/otus-golang/hw1
+
+require (
+	github.com/beevik/ntp v0.2.0
+	golang.org/x/net v0.0.0-20191126235420-ef20fe5d7933 // indirect
+)
---
+++ hw1/main.go
@@ -0,0 +1,16 @@
+package hw1
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+)
+
+func currentTime() error {
+	const server = "0.pool.ntp.org"
+	time, err := ntp.Time(server)
+	if err != nil {
+		return fmt.Errorf("Error: %s", err.Error())
+	}
+	fmt.Printf("Current time is: %s", time)
+	return nil
+}

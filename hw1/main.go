93978da3342e2bdc0cb98efedbf5665470e5f909
astepanov <stepanov.alexey@gtrf.ru>
2019-12-03 11:39:57 +0300

    add some fix

--- hw1/main.go
+++ hw1/main.go
@@ -9,8 +9,8 @@ func CurrentTime() error {
 	const server = "0.pool.ntp.org"
 	time, err := ntp.Time(server)
 	if err != nil {
-		return fmt.Errorf("Error: %s", err.Error())
+		return fmt.Errorf("Error: %s \n", err.Error())
 	}
-	fmt.Printf("Current time is: %s", time)
+	fmt.Printf("Current time is: %s \n", time.Clock())
 	return nil
 }

57d066be5db0a2def4620cb8477ba3775ae0bf83
Aleksey Stepanov <a.i.stepanov@yandex.ru>
2019-12-15 12:31:58 +0300

    some fixes

--- hw3/unpacker.go
+++ hw3/unpacker.go
@@ -5,41 +5,37 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func Unpack(s string) (string, error) {
-	var unpack []string
+	var unpack strings.Builder
 	var flagEscape bool
-	for i, value := range strings.Split(s, "") {
+	for i, value := range s {
 		switch {
-		case value == `\` && !flagEscape:
+		case string(value) == `\` && !flagEscape:
 			flagEscape = true
 		case flagEscape:
-			unpack, flagEscape = append(unpack, value), false
-		case isNumber(value) && len(unpack) > 0:
-			number, _ := strconv.Atoi(value)
+			unpack.WriteRune(value)
+			flagEscape = false
+		case unicode.IsDigit(value) && len(unpack.String()) > 0:
+			number, _ := strconv.Atoi(string(value))
 			if number == 0 {
-				unpack = unpack[:len(unpack)-1]
+				str := []rune(unpack.String())
+				str = str[:len(str)-1]
+				unpack.Reset()
+				unpack.WriteString(string(str))
 			} else {
-				w := unpack[len(unpack)-1]
-				for a := 0; a < number-1; a++ {
-					unpack = append(unpack, w)
-				}
+				w := []rune(unpack.String())
+				w = w[len(w)-1:]
+				unpack.WriteString(strings.Repeat(string(w), number-1))
 			}
-		case !isNumber(value):
-			unpack = append(unpack, value)
+		case !unicode.IsDigit(value):
+			unpack.WriteRune(value)
 		default:
-			return "", errors.New(fmt.Sprintf("Something strange with unpacking string '%s' on character - %s, position - %d.", s, value, i))
+			return "", errors.New(fmt.Sprintf("Something strange with unpacking string '%s' on character - %s, position - %d.", s, string(value), i))
 		}
 	}
-	return strings.Join(unpack, ""), nil
-}
-
-func isNumber(s string) bool {
-	_, err := strconv.Atoi(s)
-	if err != nil {
-		return false
-	}
-	return true
+	return unpack.String(), nil
 }
 

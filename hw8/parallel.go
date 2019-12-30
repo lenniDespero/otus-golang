13a8af4c13b0121f9aa566250c8eaa1b8d0b54c5
astepanov <stepanov.alexey@gtrf.ru>
2019-12-30 13:33:15 +0300

    add hw8

--- hw8/parallel.go
+++ hw8/parallel.go
@@ -1,5 +1,67 @@
 package hw8
 
-func main() {
+//WorkerFunction keep functions to workers
+type WorkerFunction func() error
 
+//Run slice of functions on N workers while get M errors
+func Run(functions []WorkerFunction, workersCount int, maxErrors int) ([]error, int) {
+	tasksChan := make(chan WorkerFunction)
+	resultsChan := make(chan error, workersCount-1)
+	closeChan := make(chan bool)
+	closedChan := make(chan bool)
+	var counter int
+	var errorsSlice []error
+
+	for i := 0; i < workersCount; i++ {
+		go startWorker(tasksChan, resultsChan, closeChan, closedChan)
+	}
+
+	func(counter *int, errorsSlice *[]error) {
+		var errors int
+		var inProgress int
+
+		for i := 0; i < workersCount; i++ {
+			inProgress++
+			tasksChan <- functions[i]
+		}
+
+		for {
+			err := <-resultsChan
+			inProgress--
+			*counter++
+
+			if err != nil {
+				*errorsSlice = append(*errorsSlice, err)
+				errors++
+			}
+
+			if *counter == len(functions) || errors == maxErrors {
+				close(closeChan)
+				return
+			} else if len(functions)-*counter-inProgress > 0 {
+				inProgress++
+				tasksChan <- functions[workersCount-1+*counter]
+			}
+		}
+	}(&counter, &errorsSlice)
+
+	for i := 0; i < workersCount; i++ {
+		<-closedChan
+	}
+	return errorsSlice, counter
+}
+
+func startWorker(tasksChan <-chan WorkerFunction, resultsChan chan<- error, closeChan <-chan bool, closedChan chan<- bool) {
+	defer func() {
+		closedChan <- true
+	}()
+
+	for {
+		select {
+		case task := <-tasksChan:
+			resultsChan <- task()
+		case <-closeChan:
+			return
+		}
+	}
 }
